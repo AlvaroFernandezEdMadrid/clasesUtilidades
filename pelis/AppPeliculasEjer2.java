24261b9d4328b7131dbde891fdb0c14fecb45173
olivia <olivia.postigo@educa.madrid.org>
2024-04-11 18:17:17 +0200

    Jueves2

---
+++ pelis/AppPeliculasEjer2.java
@@ -0,0 +1,10 @@
+package colecciones;
+
+public class AppPeliculasEjer2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ pelis/TipoPelicula.java
@@ -0,0 +1,18 @@
+package colecciones;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public enum TipoPelicula {
+	AVENTURA,COMEDIA,TERROR,CIENCIAFICCION;
+	
+	static List<String> toStringList ()
+	{
+		List <String> lista = new ArrayList<>();
+	
+		for (TipoPelicula tipo : TipoPelicula.values())
+			lista.add(tipo.toString());
+		
+		return lista;
+	}
+}
